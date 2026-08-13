a2a2b8de97aae7db05474d4684feccef0703c5cf
Andres Jimenez <andres.felipe.jimenezg13@gmail.com>
2026-08-12 22:54:43 -0500

    Agregue variable de correo y modifique el HTML para mostrar ambos datos

--- app.py
+++ app.py
@@ -2,11 +2,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-name = "Elba Zurita"
+name = "Andres"
+email = "ajime@estudiante.com"  # Tu correo
 
 @app.route("/")
 def hello_world():
-    return render_template('index.html', person=name)
+    return render_template('index.html', person=name, contacto=email)
 
 if __name__ == "__main__":
     app.run(debug=True)
